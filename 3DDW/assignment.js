3c2a054c3e2b4a935be11a474ad0df16399d7243
charly <charlyp@scual.es>
2015-11-14 22:08:39 +0100

    Category page stable

--- 3DDW/assignment.js
+++ 3DDW/assignment.js
@@ -4,6 +4,9 @@
 var category_template, photos_template, photo_template, slideshow_template;
 
 
+// variables to store the current displayed album and photo
+var current_album = animals_data.category[0];
+var current_photo = current_album.animals[0];
 
 // a helper function that instantiates a template
 // and displays the results in the content div
@@ -20,12 +23,78 @@ $(document).ready(function(){
 	var source   = $("#category-template").html();
 	category_template = Handlebars.compile(source);
 
+	source   = $("#photos-template").html();
+	photos_template = Handlebars.compile(source);
+
+	//source   = $("#photo-template").html();
+	//photo_template = Handlebars.compile(source);
+
 	$("#category-tab").click(function () {
 
 		console.log("category-tab");
-		// displays the albums template
+		// displays the category template
 		showTemplate(category_template, animals_data);
+				// make the category tab the active one
+		// first make the currently active tab inactive
+		$(".nav-tabs .active").removeClass("active");
+		// then make category tab active
+		$("#category-tab").addClass("active");
+
+		
+
+		// add a click callback to each album 
+		// thumbnail which displays the photos
+		// template on that album
+		// (I have written out the code for this 
+		// function for clarity but it is actually
+		// pretty much the same as the photos tab
+		// function so we could acutally just
+		// call $(".photo-thumbnail").click() ) 
+		// 		$(".album-thumbnail").click(function (){
+			$(".link-animals").click(function (){
+			// get the index (position in the array)
+			// of the album we clicked on
+			// "this" is the element that was clicked on
+			// data("id") gets the attribute data-id
+			// (which we set to the index of the album in
+			// the array - @index)
+			var index = $(this).data("id");
+			console.log(index);
+
+			// set the current album to this album
+			current_album = animals_data.category[index];
+
+			// displays the photos template
+			showTemplate(photos_template, current_album);
+
+			// add an on click al all the photo thumbnails
+			// which displays the photo in a modal popup
+			$(".photo-thumbnail").click(function (){
+				// get the index (position in the array)
+				// of the photo we clicked on
+				// "this" is the element that was clicked on
+				// data("id") gets the attribute data-id
+				// (which we set to the index of the photo in
+				// the array - @index)
+				var index = $(this).data("id");
+
+				// set the current photo to this photo
+				current_photo = current_album.photos[index];
+				
+				// displays the single photo template
+				showTemplate(photo_template, current_photo);
+			});
+		});
+			
 	});
+
+	// 
+	//  clicking on the photos tab shows all of the 
+	//  photos in the current album
+	//
+
+
+	$("#category-tab").click();
 });
 
 
